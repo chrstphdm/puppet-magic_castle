b2db67bffa9174d7a3c2dcf511d4e6ff2757b22f
chrstphdm <7338028+chrstphdm@users.noreply.github.com>
2025-06-16 18:17:21 +0200

    fix: Update mount command path in S3FS manifest to ensure proper execution

--- site/profile/manifests/s3fs.pp
+++ site/profile/manifests/s3fs.pp
@@ -51,8 +51,7 @@ class profile::s3fs (
     # Mount the S3 bucket using S3FS
     exec { "mount_s3fs_${name}":
       command => "s3fs ${bucket} ${mountpoint} -o passwd_file=${credfile} -o url=https://${region}.s3.cloud.ovh.net -o use_path_request_style",
-      unless  => "mount | grep -q '${mountpoint}'", # Prevent remounting if already mounted
-      path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'], # Ensure 'mount' command is found
+      unless  => "/bin/mount | grep -q '${mountpoint}'", # Prevent remounting if already mounted
       require => [ File[$credfile], File[$mountpoint], Package['s3fs-fuse'] ], # Ensure dependencies are met
     }
   }
